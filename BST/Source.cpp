28d2e6ac08f0ad651f1691b4abd22d785ff658c0
Youssef Raafat <YoussefRaafatNasry@gmail.com>
2018-11-28 02:00:51 +0200

    Update Source.cpp
     * Create a balanced tree from an array then Visualize it.

--- BST/Source.cpp
+++ BST/Source.cpp
@@ -5,24 +5,17 @@ using namespace std;
 
 void main()
 {
-	BST<int> B;
+	BST<int> Tree;
+	int arr[13] = { 10, 7, 9, 6, 1, 8, 11, 2, 0, 4, 5, 3, 12 };
 
-	B.Insert(63);
-	B.Insert(41);
-	B.Insert(74);
-	B.Insert(16);
-	B.Insert(53);
-	B.Insert(65);
-	B.Insert(25);
-	B.Insert(46);
-	B.Insert(55);
-	B.Insert(64);
-	B.Insert(70);
-	B.Insert(11);
-	B.Insert(81);
+	// Create a balanced tree from the previous array
+	Tree = Tree.CreateBalancedTree(arr, 13);
 
+	// Visualize the resulting tree
+	Tree.VisualizeTree();
 
-	B.VisualizeTree();
+	cout << endl << endl;
 
-	system("pause");
+	system("Pause");
+
 }
