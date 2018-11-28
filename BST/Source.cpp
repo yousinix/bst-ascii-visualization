d2ef3957957ac603c5244e7f616407b1b33fb23e
Youssef Raafat <YoussefRaafatNasry@gmail.com>
2018-11-28 18:03:42 +0200

    Update Source.cpp
     * Add title to the console.
     * Add more elements to the array.
     * Calculate array size automatically.
     * Rearrange code.

--- BST/Source.cpp
+++ BST/Source.cpp
@@ -1,15 +1,26 @@
 #include <iostream>
+#include <Windows.h>
 #include "BST.cpp"
 
 using namespace std;
 
 void main()
 {
+	SetConsoleTitle("Binary Search Tree");
+
+	// Initialize Array
+	int arr[] = { 10, 7, 9, 6, 1, 19, 20, 21, 22, 23, 8, 11, 2, 0, 28, 29,
+		          4, 5, 3, 12, 13, 14, 15, 16, 17, 18, 24, 25, 26, 27, 30 };
+
+	// Get Array size
+	int arrSize = sizeof(arr) / sizeof(arr[0]);
+
+	cout << endl << endl;
+
 	BST<int> Tree;
-	int arr[13] = { 10, 7, 9, 6, 1, 8, 11, 2, 0, 4, 5, 3, 12 };
 
 	// Create a balanced tree from the previous array
-	Tree = Tree.CreateBalancedTree(arr, 13);
+	Tree = Tree.CreateBalancedTree(arr, arrSize);
 
 	// Visualize the resulting tree
 	Tree.VisualizeTree();
